0f200002d0dfa9468484424b921b9130ac338940
ivanbeltransegovia <139892706+ivanbeltransegovia@users.noreply.github.com>
2023-08-11 12:01:42 -0600

    Add files via upload
    practica 1

---
+++ practica1-ivan/practica1.js
@@ -0,0 +1,35 @@
+
+// let edad=prompt("introduzca su edad: ")
+
+// if(edad > 18 && edad <= 100){
+//     alert("el usuario es mayor de edad")
+// }
+// else if(edad<18 && edad >=1){
+//      alert("el usuario es menor de edad")
+// }
+// else if(edad==18){
+//     alert("el usuario tiene 18")
+// }
+// else if(edad>100){
+//     alert("ya esta viejito necesita un diseño de autor")
+// }
+// else if(edad<=0){
+//     alert("edad invalida")
+// }
+
+/////////////////ejercicio2
+
+// let i=1;
+// let suma=0;
+
+// while(i<=100){
+//     suma+=i;
+//     i++;
+// }
+// alert(suma)
+///////////////////////////ejerjcicio3/////
+let num=prompt("escriba un numero : ")
+
+for(i=1; i<=10; i++){
+    alert(num+" por " +  i+  "  es igual a :"+(num*1))
+}
